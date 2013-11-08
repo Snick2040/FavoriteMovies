8fac526b59a471c2d12a170b0f2f337dd6148375
Snick2040 <nick@Nicks-MacBook-Pro.local>
2013-11-07 19:43:02 -0500

    Initial Commit

---
+++ FavoriteMovies/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FavoriteMovies
+//
+//  Created by Nick on 11/7/13.
+//  Copyright (c) 2013 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FavoriteMovies/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FavoriteMovies
+//
+//  Created by Nick on 11/7/13.
+//  Copyright (c) 2013 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
